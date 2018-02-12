9d117fa5d20b2aa2dcc2634aa6c84598f219dde2
Lucas Federico Torroba Hennigen <lucasfth@gmail.com>
2018-02-12 16:23:26 +0000

    feat: add debug info

--- flaskapp/updates.sh
+++ flaskapp/updates.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+echo "User: "
+whoami
+echo "Current dir: "
+pwd
+
 cd /var/www/html/production/ && git fetch --all && git reset --hard origin/master
 cd /var/www/html/development/ && git fetch --all && git reset --hard origin/development
